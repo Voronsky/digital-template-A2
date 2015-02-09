d93e083ff00683e0852ea793632226253a8bf916
Ivan Diaz <idiaz3@gmu.edu>
2015-02-09 18:59:52 -0500

    Added basic functionality

--- js/main.js
+++ js/main.js
@@ -6,16 +6,98 @@ var game = new Phaser.Game(900, 675, Phaser.AUTO, 'game', {preload: preload, cre
 function preload() {
 
 game.load.audio('music',['assets/audio/calvin_harris_school.mp3']);
+game.load.audio('jump',['assets/audio/mario_jump.mp3');
 game.load.image('sky',['assets/sky.png']);
 game.load.image('ground',['assets/platform.png']);    
 game.load.image('dude',['assets/dude.png']);
 
 }
 
+var player;
+var enemy;
+var platforms;
+var cursors;
+var cats;
+var music;
+var jump;
+var isDead = false;
+
 function create(){
+    music = game.add.audio('music');
+    music.volume = 0.3;
+    music.loop = true;
+    music.play();
+    jump = game.add.audio('jump');
+    
+    //Physics time!
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    //The background
+    game.add.sprite(0,0,'sky');
+    
+    //adding platforms and it's physics
+    platforms = game.add.group();
+    platforms.enableBody = true;
+   
+    var ground = platforms.create(0, game.world.height - 64, 'ground');
+
+    //Scale it to fit the game width and such
+    ground.scale.setTo(3,3);
+    ground.body.immovable = true;
+    
+    //Ledge time
+    var ledge = platforms.create(450, 400, 'ground');
+    ledge = platforms.create(450, -150, 'ground');
+    ledge = platforms.create(300, 150, 'ground');
+    ledge.body.immovable = true;
+    
+    //Making them players
+    player = game.add.sprite(32, game.world.height - 150, 'dude');
+    game.physics.arcade.enable(player);
+   
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y  = 400;
+    player.body.collideWorldBounds = true;
+    
+    //Adding their animations
+    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    player.animations.add('right', [4, 5, 6, 7, 8], 10, true);
+
 
 }
 
 function update(){
+    //Constantly update them game physics doe
+    game.physics.arcade.collide(player, platforms);
+    
+    
+    player.body.velocity.x = 0;
+
+    if (cursors.left.isDown)
+    {
+	player.body.velocirty.x = -175;
+	player.animations.player('left');
+    }
+    else if
+    {
+	player.body.velocity.x = 175;
+	player.animations.play('right');
+    }
+    else
+    {
+	player.animations.stop();
+	
+	player.frame = 4;
+    }
+    
+    //Jump functions
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+	jump.volume = 0.5;
+	jump.play();
+	player.body.velocity.y = -425;
+
+
+    }
 
 }
